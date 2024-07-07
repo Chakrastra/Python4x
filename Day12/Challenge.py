497f05215f2a5e42d50a91ad8ae05c6551ed2a9d
chakrastra <rajakvivek080@gmail.com>
2024-07-07 17:36:14 +0530

    Numpy

---
+++ Day12/Challenge.py
@@ -0,0 +1,13 @@
+#Oprations using numpy 
+
+import numpy as np
+
+arr = np.random.randint(1, 101, size=10)
+
+max = np.max(arr)
+
+min = np.min(arr)
+
+print("Random array:", arr)
+print("Maximum value:", max)
+print("Minimum value:", min)
